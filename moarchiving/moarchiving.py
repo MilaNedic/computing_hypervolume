73618639c462fd62110252daa020460f9323942f
MilaNedic <roselle.freya@gmail.com>
2024-03-07 13:20:32 +0100

    Create moarchiving.py

---
+++ moarchiving/moarchiving.py
@@ -0,0 +1,14 @@
+"""This module contains a MOO archive class for n=3 and n=4 dimnesions
+    for calculating the hypervolume.
+"""
+
+inf = float('inf')
+
+class moarchiving(list):
+    
+    def __init__(self,
+             list_of_f_pairs=None,
+             reference_point=None,
+             sort=sorted):
+        
+        pass
