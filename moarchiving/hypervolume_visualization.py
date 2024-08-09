c5e20093c225c640c809c42437ead52367be032a
Nace Sever <47631585+naceee@users.noreply.github.com>
2024-08-09 11:25:45 +0200

    visualizations of hypervolume in 3d

---
+++ moarchiving/hypervolume_visualization.py
@@ -0,0 +1,143 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
+from moarchiving import MOArchive
+from moarchiving2d import BiobjectiveNondominatedSortedList
+
+
+def main():
+    fig = go.Figure(data=[
+        go.Mesh3d(
+            # 8 vertices of a cube
+            x=[0, 0, 1, 1, 0, 0, 1, 1],
+            y=[0, 1, 1, 0, 0, 1, 1, 0],
+            z=[0, 0, 0, 0, 1, 1, 1, 1],
+            # Intensity of each vertex, which will be interpolated and color-coded
+            # i, j and k give the vertices of triangles
+            i = [7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+            j = [3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+            k = [0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+            name='y',
+            showscale=True
+        )
+    ])
+
+    # add a scatter plot to the figure
+    fig.add_trace(go.Scatter3d(
+        x=[0, 1],
+        y=[0, 1],
+        z=[0, 1],
+        mode='markers',
+        marker=dict(
+            size=12,
+            color='rgb(0, 0, 0)',                # set color to black
+            symbol='circle',                    # set symbol to circle
+            line=dict(
+                color='rgb(0, 0, 0)',            # set line color to black
+                width=0.5
+            )
+        )
+    ))
+
+    fig.show()
+
+
+def draw_point(point, ref_point, fig):
+    # draw a cube with the point as one of the vertices and the ref_point as the opposite vertex
+    fig.add_trace(go.Mesh3d(
+        # 8 vertices of a cube
+        x=[point[0], point[0], ref_point[0], ref_point[0], point[0], point[0], ref_point[0], ref_point[0]],
+        y=[point[1], ref_point[1], ref_point[1], point[1], point[1], ref_point[1], ref_point[1], point[1]],
+        z=[point[2], point[2], point[2], point[2], ref_point[2], ref_point[2], ref_point[2], ref_point[2]],
+        # Intensity of each vertex, which will be interpolated and color-coded
+        # i, j and k give the vertices of triangles
+        i=[7, 0, 0, 0, 4, 4, 6, 6, 4, 0, 3, 2],
+        j=[3, 4, 1, 2, 5, 6, 5, 2, 0, 1, 6, 3],
+        k=[0, 7, 2, 3, 6, 7, 1, 1, 5, 5, 7, 6],
+        name='y',
+        showscale=True
+    ), row=1, col=1)
+
+
+def draw_hypervolume(points, ref_point):
+    points = points.tolist()
+    moa = MOArchive(points, ref_point)
+    points = moa.points_list
+
+    fig = make_subplots(rows=1, cols=1,
+                        specs=[[{'type': 'surface'}]])
+
+    # add a scatter plot to the figure
+    fig.add_trace(go.Scatter3d(
+        x=[ref_point[0]],
+        y=[ref_point[1]],
+        z=[ref_point[2]],
+        mode='markers',
+        marker=dict(
+            size=8,
+            color='rgb(160, 160, 160)',                # set color to black
+            symbol='circle',                    # set symbol to circle
+        ),
+        name="ref_point"
+    ), row=1, col=1)
+
+    for p in points:
+        draw_point(p, ref_point, fig)
+        fig.add_trace(go.Scatter3d(
+            x=[p[0]],
+            y=[p[1]],
+            z=[p[2]],
+            mode='markers',
+            marker=dict(
+                size=4,
+                color='rgb(0, 0, 0)',                # set color to black
+                symbol='x',                    # set symbol to circle
+                line=dict(
+                    color='rgb(0, 0, 0)',            # set line color to black
+                    width=0.5
+                )
+            ),
+            name=str(p)
+        ), row=1, col=1)
+    eps = ref_point[0] / 100
+
+    kink_points2 = moa._get_kink_points_tea()
+    kink_points = moa._get_kink_points()
+
+    """
+    for k2 in kink_points2:
+        assert k2 in kink_points, f"{k2} not in {kink_points}"
+    for k in kink_points:
+        assert k in kink_points2, f"{k} not in {kink_points2}"
+    """
+
+    for k in kink_points:
+        fig.add_trace(go.Scatter3d(
+            x=[k[0] - eps],
+            y=[k[1] - eps],
+            z=[k[2] - eps],
+            mode='markers',
+            marker=dict(
+                size=10,
+                color='rgb(0, 0, 0)',
+                symbol='circle',
+            ),
+            name=str(k)
+        ), row=1, col=1)
+
+    fig.show()
+
+
+def random_3d_structure(n_points):
+    points = np.random.rand(n_points, 3)
+    ref_point = [1, 1, 1]
+    draw_hypervolume(points, ref_point)
+
+
+if __name__ == '__main__':
+    for i in range(3):
+        random_3d_structure(10)
+    pts = np.array(
+        [[1, 2, 3], [2, 3, 1], [3, 1, 2]]
+    )
+    draw_hypervolume(pts, [6, 6, 6])
