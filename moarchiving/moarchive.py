e59877f6f3478ad8e4e0383dd091f61dad29976d
Nace Sever <47631585+naceee@users.noreply.github.com>
2024-09-11 11:12:36 +0200

    small rename

--- moarchiving/moarchive.py
+++ moarchiving/moarchive.py
@@ -3,7 +3,7 @@ from moarchiving3d import MOArchive3d
 from moarchiving4d import MOArchive4d
 
 
-def MOArchive(list_of_f_vals=None, ref_point=None, infos=None, n_obj=None):
+def MOArchive(list_of_f_vals=None, reference_point=None, infos=None, n_obj=None):
     assert list_of_f_vals is not None or n_obj is not None, \
         "Either list_of_f_vals or n_obj must be provided"
     if list_of_f_vals is not None and n_obj is not None:
@@ -13,11 +13,11 @@ def MOArchive(list_of_f_vals=None, ref_point=None, infos=None, n_obj=None):
         n_obj = len(list_of_f_vals[0])
 
     if n_obj == 2:
-        return MOArchive2d(list_of_f_vals, ref_point)
+        return MOArchive2d(list_of_f_vals, reference_point)
     elif n_obj == 3:
-        return MOArchive3d(list_of_f_vals, ref_point, infos)
+        return MOArchive3d(list_of_f_vals, reference_point, infos)
     elif n_obj == 4:
-        return MOArchive4d(list_of_f_vals, ref_point, infos)
+        return MOArchive4d(list_of_f_vals, reference_point, infos)
     else:
         raise ValueError(f"Unsupported number of objectives: {n_obj}")
 
